329ad0436bd303aa0cca5ed3e0b4b03120e7bbde
mariashelin <mariashelina2004@gmail.com>
2025-08-16 17:06:11 +0700

    Menambahkan teks baru

--- contoh-projek.py
+++ contoh-projek.py
@@ -2,4 +2,6 @@ print("hello world this is mariashelin")
 print("ini adalah contoh projek python untuk tugas eksplorasi github untuk matkul ppl prak")
 print("udah selesai capek banget")
 print("1234567890")
+print("eh besok ada tugas lagi")
+
 
