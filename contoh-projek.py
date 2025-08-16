af1f70a17aed5aadbb1547eb2501cb3240d2b494
mariashelin <mariashelina2004@gmail.com>
2025-08-16 16:43:41 +0700

    Test verified commit

--- contoh-projek.py
+++ contoh-projek.py
@@ -1,2 +1,2 @@
 print("hello world this is mariashelin")
-print("ini adalah contoh projek python untuk tugas eksplorasi github")
+print("ini adalah contoh projek python untuk tugas eksplorasi github hehe")
